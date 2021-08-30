89c62626c9fc6a33267505941229b0ec2b543c65
Pedro Wilson <pdro.wilson@gmail.com>
2021-08-30 14:47:17 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Let's build slack</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBnJCmVizBvZWlY4F2syFjWX0Ni4UjvjqY",
+  authDomain: "slack-clone-f0d19.firebaseapp.com",
+  projectId: "slack-clone-f0d19",
+  storageBucket: "slack-clone-f0d19.appspot.com",
+  messagingSenderId: "238641722763",
+  appId: "1:238641722763:web:bfe2392dc3f7f0e0352b8e"
+};
