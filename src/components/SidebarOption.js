c923b62a42b51fcddd647bea8b60557d91056936
Pedro Wilson <pdro.wilson@gmail.com>
2021-08-30 19:57:09 -0300

    setting up firebase.js

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,9 +1,22 @@
 import styled from "styled-components"
+import { db } from '../firebase'
 
-function SidebarOption({ Icon, title }) {
+function SidebarOption({ Icon, title, addChannelOption }) {
+    const addChannel = () => {
+        const channelName = prompt('Please enter the channel name')
+
+        if (channelName) {
+            db.collection('rooms').add({
+                name: channelName 
+            })
+        }
+    }
+    const selectChannel = () => {}
  
     return (
-        <SidebarOptionContainer>
+        <SidebarOptionContainer
+            onClick={ addChannelOption ? addChannel : selectChannel }
+        >
             {Icon && <Icon fontSize="small" style={{padding: 10}} />}
             {Icon ? (
                 <h3>{title}</h3>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,10 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase"
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBnJCmVizBvZWlY4F2syFjWX0Ni4UjvjqY",
   authDomain: "slack-clone-f0d19.firebaseapp.com",
@@ -6,3 +13,10 @@ const firebaseConfig = {
   messagingSenderId: "238641722763",
   appId: "1:238641722763:web:bfe2392dc3f7f0e0352b8e"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebaseApp.firestore();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider }
