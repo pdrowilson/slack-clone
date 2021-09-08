eadbae687c09c2ed78b5021e881460bd76656497
Pedro Wilson <pdro.wilson@gmail.com>
2021-09-08 00:21:21 -0300

    setting up eslint/prettier and editorconfig

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Header from './components/Header'
 import Sidebar from './components/Sidebar'
-import Chat from  './components/Chat'
+import Chat from './components/Chat'
 
-import styled from "styled-components";
-import './App.css';
+import styled from 'styled-components'
+import './App.css'
 
 function App() {
   return (
@@ -26,12 +22,12 @@ function App() {
         </AppBody>
       </>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
 
 const AppBody = styled.div`
   display: flex;
   height: 100vh;
-`;
+`
